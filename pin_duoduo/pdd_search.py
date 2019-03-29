73349dfbba7854f914cdf316d8b5a48cdae39fb2
onetwo1 <30581649+onetwo1@users.noreply.github.com>
2019-03-29 12:52:27 +0800

    Update pdd_search.py

--- pin_duoduo/pdd_search.py
+++ pin_duoduo/pdd_search.py
@@ -9,11 +9,6 @@ url = "https://mobile.yangkeduo.com/proxy/api/search?"
 
 referer_page_id = f"10031_{str(get_timestamp())}_{get_random_string(10)}"
 
-referer_page_url = f"https://mobile.yangkeduo.com/search_result.html?search_key=" \
-                   "%E6%96%AF%E5%87%AF%E5%A5%87%20%E7%86%8A%E7%8C%AB%E9%9E%8B&" \
-                   "search_src=history&search_met=history_sort&search_met_track" \
-                   "=history&refer_page_name=search&refer_page_id" \
-                   "={}&refer_page_sn=10031".format(referer_page_id)
 headers = {
     'accept': "*/*",
     'accept-encoding': "gzip, deflate, br",
@@ -24,7 +19,7 @@ headers = {
                   "(KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36",
     'cache-control': "no-cache",
 }
-anti_content = os.popen(f'node {JS_FILE_PATH} "{referer_page_url}"').read().strip()
+anti_content = os.popen(f'node {JS_FILE_PATH}').read().strip()
 group_params = dict(
     page=7,
     size=50,
@@ -34,7 +29,6 @@ group_params = dict(
     list_id=f"{get_random_string(10)}",
     anti_content=str(anti_content),
     pdduid=0,
-    # flip="40;2;0;20;46a6fb08-3519-4cd4-bcbd-af9fbf458646"
 )
 
 res = requests.get(url, params=group_params, headers=headers)
